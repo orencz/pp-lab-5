09494581c434232737a1c39e2539ffcc3322fef6
orencz <xxorenczxxgaming@gmail.com>
2024-05-04 12:23:57 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        
+        int sum = MathUtils.add(10, 21);
+        
+        System.out.println("Wynikiem dodawania 10 + 21 jest: " + sum);
+        
+        
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Testowa Wiaodmosc");
+        messenger.sendMessage("Wynikiem dodawania 10 + 21 jest: " + sum);
     }
 }
